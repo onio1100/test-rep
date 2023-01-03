37210ab19487c1b0f30b953c6e451b9cac5f38b5
onio1100 <nonio1100n@gmail.com>
2023-01-03 22:31:03 +0100

    section 2 added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // section 1
+
 const button1 = document.getElementById("button-1");
 let r1 = 0;
 function flip(){
@@ -7,3 +8,19 @@ function flip(){
     console.log("test");
 }
 button1.addEventListener("click", flip);
+
+// section 2
+
+const square2 = document.getElementById("square-2");
+const widthSlider2 = document.getElementById("width-2");
+const heightSlider2 = document.getElementById("height-2");
+
+function width2(){
+    square2.style.width = widthSlider2.value + "px";
+}
+function height2(){
+    square2.style.height = heightSlider2.value + "px";
+}
+
+widthSlider2.addEventListener("input", width2);
+heightSlider2.addEventListener("input",height2);
