cc502d13df1165797b92dbaa8f459c861e4e006c
onio1100 <nonio1100n@gmail.com>
2023-01-04 21:37:06 +0100

    section 3 added

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 
 const button1 = document.getElementById("button-1");
 let r1 = 0;
-function flip(){
+function flip1(){
     r1 += 360;
     button1.style.transform = "rotate(" + r1 + "deg)";
-    console.log("test");
 }
-button1.addEventListener("click", flip);
+button1.addEventListener("click", flip1);
 
 // section 2
 
@@ -24,3 +23,31 @@ function height2(){
 
 widthSlider2.addEventListener("input", width2);
 heightSlider2.addEventListener("input",height2);
+
+// section 3
+
+const display3 = document.getElementById("display-3");
+const mainButton3 = document.getElementById("main-button-3");
+const button13 = document.getElementById("button-1-3");
+let clicks3 = 0;
+let controlValue3 = 0;
+
+function plus3(){
+    clicks3++;
+    controlValue3++;
+}
+
+function add3(){
+    plus3();
+    display3.innerHTML = clicks3;
+}
+
+// function showAuto3(){
+//     if(clicks3 == 10){
+//         button13.classList.add("");
+//         display3.removeEventListener("change", showAuto3);
+//     };
+//     console.log("test");
+// }
+
+mainButton3.addEventListener("click", add3);
