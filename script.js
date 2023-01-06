a908ae4b6d4af039d656ad06a04a54330d01aead
onio1100 <nonio1100n@gmail.com>
2023-01-06 23:15:08 +0100

    section 3 update

--- script.js
+++ script.js
@@ -30,13 +30,17 @@ const display3 = document.getElementById("display-3");
 const mainButton3 = document.getElementById("main-button-3");
 const button13 = document.getElementById("button-1-3");
 const button23 = document.getElementById("button-2-3");
+let plusValue = 1;
 let clicks3 = 0;
-let multiplier3 = [0];
-let controlValue = [10, button13, autoClick3,
-[100, button23, double3]];
+let multiplier3 = [0, 0];
+let controlValue = 
+[10, button13, autoClick3,
+    [100, button23, double3, [
+        
+]]];
 
 function plus3(){
-    clicks3++;
+    clicks3 += plusValue;
     display3.innerHTML = clicks3;
     control(controlValue);
 }
@@ -61,7 +65,7 @@ function unlockButton3(buttonName, functionName){
 function multiplierControl3(element, index){
     multiplier3[index]++;
     element.firstElementChild.classList.add("multiplier-3-on");
-    element.firstElementChild.innerHTML = multiplier3[0];
+    element.firstElementChild.innerHTML = multiplier3[index];
 }
 
 function autoClick3(){
@@ -73,13 +77,10 @@ function autoClick3(){
 }
 
 function double3(){
-    if(clicks3 > 49){
-        minus3(50);
-        // multiplierControl3(button23, 1);
-        if(clicks3 != 0){
-            clicks3 = clicks3 * 2;
-            display3.innerHTML = clicks3;
-        }
+    if(clicks3 > 99){
+        minus3(100);
+        multiplierControl3(button23, 1);
+        plusValue++;
     }
 };
 
